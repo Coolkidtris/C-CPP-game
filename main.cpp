cf9fb08a704e5d11a81a17cd883337e9c7ca034b
Tristan COWEN <Tristancowen@hotmail.com>
2025-01-22 15:55:00 +0000

    #3
    Added name input and output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+char name[256];
+
 int main(){
-    std::cout << "Hello, World";
+    std::cout << "What is your name?\n";
+    std::cin >> name;
+    std::cout << "Hello," << name;
     return 0;
 }
